a1eb2083c1adbf7a001fc4970fdf8360fec07278
Guilherme Matheus de souza <gui_souza_15@hotmail.com>
2021-03-02 19:48:38 -0300

    Initial commit

---
+++ silentInstallerByJson.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace sillentInstaller
+{
+    partial class silentInstallerByJson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(12, 309);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(263, 23);
+            this.btnInstall.TabIndex = 0;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // silentInstallerByJson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInstall);
+            this.Name = "silentInstallerByJson";
+            this.Text = "silentInstallerByJson";
+            this.Load += new System.EventHandler(this.silentInstallerByJson_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInstall;
+    }
+}
---
+++ silentInstallerByJson.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sillentInstaller
+{
+    public partial class silentInstallerByJson : Form
+    {
+        public silentInstallerByJson()
+        {
+            InitializeComponent();
+        }
+
+
+        private void silentInstallerByJson_Load(object sender, EventArgs e)
+        {
+            int width = this.Width;
+            int heigth = this.Height;
+            int lastWidth = 30;
+            int lastHeigth = 30;
+
+            using (StreamReader r = new StreamReader("config.json"))
+            {
+                string json = r.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(json);
+                foreach (var item in array.APPS)
+                {
+
+                    CheckBox chk = new CheckBox();
+                    chk.Text = item.name;
+                    chk.Location = new System.Drawing.Point( lastWidth, lastHeigth);
+                    chk.MaximumSize = new Size(100,100);
+                    this.Controls.Add(chk);
+                    lastWidth += 100;
+                    if(lastWidth >= width)
+                    {
+                        lastHeigth += 30;
+                        lastWidth = 30;
+                    }
+                        
+                    
+                   
+                }
+
+            }
+           
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            dynamic array;
+            using (StreamReader r = new StreamReader("config.json"))
+            {
+                string json = r.ReadToEnd();
+                array = JsonConvert.DeserializeObject(json);
+
+            }
+            
+
+            foreach (Control c in Controls)
+            {
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    if (cb.Checked )
+                    {
+                        foreach(var item in array.APPS)
+                        {
+                            if(item.name == cb.Text)
+                            {
+                                Console.WriteLine(item.url);
+                                break;
+                            }
+                        }
+                        Console.WriteLine(cb.Text);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
